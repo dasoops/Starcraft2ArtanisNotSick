6d44ad98e50b6b0231e4f7eae8993c3f61303245
Dasoops <DasoopsNicole@gmail.com>
2024-01-02 19:01:44 +0800

    feat: SettingScreenKt.SettingScreen() add VerticalScrollbar()

--- common/src/commonMain/kotlin/com/dasoops/common/screen/setting/SettingScreen.kt
+++ common/src/commonMain/kotlin/com/dasoops/common/screen/setting/SettingScreen.kt
@@ -1,12 +1,17 @@
 package com.dasoops.common.screen.setting
 
+import androidx.compose.foundation.VerticalScrollbar
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.rememberScrollbarAdapter
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Divider
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import com.dasoops.common.screen.setting.option.Appearance
@@ -17,17 +22,30 @@ val logger = KotlinLogging.logger {}
 
 @Composable
 fun SettingScreen() {
-    Column(
+    val scrollState = rememberScrollState()
+    Box(
         modifier = Modifier
-            .fillMaxWidth()
-            .padding(top = 24.dp, start = 12.dp, end = 12.dp)
-            .verticalScroll(rememberScrollState())
     ) {
-        TopAppInfo()
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(top = 24.dp, start = 12.dp, end = 24.dp)
+                .verticalScroll(scrollState)
+        ) {
+            TopAppInfo()
 
-        Divider(thickness = 1.dp, modifier = Modifier.padding(vertical = 16.dp))
+            Divider(thickness = 1.dp, modifier = Modifier.padding(vertical = 16.dp))
 
-        Appearance()
-        MapInfo()
+            Appearance()
+            MapInfo()
+        }
+        VerticalScrollbar(
+            modifier = Modifier
+                .align(Alignment.CenterEnd)
+                .fillMaxHeight()
+                .padding(top = 24.dp, end = 8.dp),
+            adapter = rememberScrollbarAdapter(scrollState),
+        )
     }
+
 }
