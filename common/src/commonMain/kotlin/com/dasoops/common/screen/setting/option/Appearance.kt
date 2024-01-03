33b8638add5ec3df7d49e6bb6a774cb39f4bb36b
Dasoops <DasoopsNicole@gmail.com>
2024-01-03 10:59:33 +0800

    update: Appearance.kt - Theme() use Common.kt.SelectOption()

--- common/src/commonMain/kotlin/com/dasoops/common/screen/setting/option/Appearance.kt
+++ common/src/commonMain/kotlin/com/dasoops/common/screen/setting/option/Appearance.kt
@@ -1,18 +1,8 @@
 package com.dasoops.common.screen.setting.option
 
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.size
-import androidx.compose.material3.DropdownMenuItem
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
 import com.dasoops.common.component.theme.Theme
 import com.dasoops.common.screen.setting.logger
 
@@ -25,33 +15,15 @@ fun Appearance() {
 @Composable
 private fun Theme() {
     var theme by setting.theme
-    Row(
-        modifier = Modifier.height(45.dp)
-    ) {
-        Description(
-            modifier = Modifier,
-            title = "Theme",
-            subTitle = "Select app color scheme",
-        )
-        Box(
-            modifier = Modifier,
-            contentAlignment = Alignment.CenterStart
-        ) {
-            Select(
-                text = { Text(text = theme.name, style = MaterialTheme.typography.labelMedium) },
-                modifier = Modifier.size(height = 30.dp, width = 160.dp),
-            ) { (_, setExpanded) ->
-                Theme.values().forEach {
-                    DropdownMenuItem(
-                        text = { Text(text = it.data) },
-                        onClick = {
-                            logger.debug { "setting.theme change -> $it" }
-                            theme = it
-                            setExpanded(false)
-                        }
-                    )
-                }
-            }
+    SelectOption(
+        title = "Theme",
+        subTitle = "Select app color scheme",
+        selectText = theme.data,
+        items = Theme.values().toList(),
+        itemText = { it.data },
+        onSelectItem = {
+            logger.debug { "setting.theme change -> $it" }
+            theme = it
         }
-    }
+    )
 }
