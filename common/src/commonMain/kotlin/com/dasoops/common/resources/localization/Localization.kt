f5cf3f81cdd189e353aa2753d21d0087b05d55a2
Dasoops <DasoopsNicole@gmail.com>
2024-01-05 11:35:46 +0800

    feat: Localization.kt - Dict

--- common/src/commonMain/kotlin/com/dasoops/common/resources/localization/Localization.kt
+++ common/src/commonMain/kotlin/com/dasoops/common/resources/localization/Localization.kt
@@ -1,14 +1,36 @@
 package com.dasoops.common.resources.localization
 
+import kotlinx.serialization.KSerializer
 import kotlinx.serialization.Serializable
 import kotlinx.serialization.Transient
+import kotlinx.serialization.builtins.MapSerializer
+import kotlinx.serialization.builtins.serializer
+import net.mamoe.mirai.utils.map
+
+@Serializable(with = Dict.Serializer::class)
+class Dict(
+    private val delegate: Map<String, String>
+) : Map<String, String> by delegate {
+    override fun get(key: String): String {
+        return delegate[key] ?: "$$$key"
+    }
+
+    private object SuperSerializer :
+        KSerializer<Map<String, String>> by MapSerializer(String.serializer(), String.serializer())
+
+    object Serializer : KSerializer<Dict> by SuperSerializer.map(
+        resultantDescriptor = SuperSerializer.descriptor,
+        serialize = { it.toMap() },
+        deserialize = { Dict(it) },
+    )
+}
 
 @Serializable
 class Localization(
     val title: String,
     val simpleTitle: String,
     val screen: ScreenText,
-    val dict: Map<String, String>,
+    val dict: Dict,
 ) {
     @Transient
     lateinit var language: Language
